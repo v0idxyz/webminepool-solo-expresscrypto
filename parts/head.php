e17792fda951c0f5b0639c2183f34be0f3edea58
v0idxyz <58184010+v0idxyz@users.noreply.github.com>
2020-09-30 14:52:01 +0200

    Create head.php

---
+++ parts/head.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="dist/favicon.ico">
+
+    <title>WebMineSOLO</title>
+
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/fontawesome.min.css" rel="stylesheet">
+    <link href="dist/css/crutch.css" rel="stylesheet">
+  </head>
+
+  <body>
